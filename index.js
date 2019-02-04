358ffc4256851a7cbf8cb12ddd5cf628cce438aa
cidro <pablo.canaleshernandez@gmail.com>
2019-02-04 12:41:57 -0300

    added caching layer to the plugin

--- index.js
+++ index.js
@@ -1,16 +1,30 @@
 import { CancelToken } from 'axios';
 import RequestManager from './lib/RequestManager';
+import CacheManager from './lib/CacheManager';
 import extend from './lib/extend';
 import lifeCycle from './lib/lifeCycle';
 
-export default function patchAxios(axios, vue, options) {
+/**
+ * @param {String} event
+ * @param {context} options
+ * @params {AxiosRequestConfig} options.config
+ * @params {Object} options.payload
+ */
+const dispatchConfigEvent = function(event, { config, payload }) {
+  if (config.hasOwnProperty(event) && typeof config[event] === 'function') {
+    config[event](payload);
+  }
+};
 
+export default function patchAxios(axios, vue, options) {
   const defaults = {
-    debug: false,
+    debug: false
   };
   const settings = extend({}, defaults, options);
 
   const requestManager = new RequestManager(settings);
+
+  const cacheManager = new CacheManager(settings);
 
   // Mount global mixin on Vue root instance
   vue.mixin(lifeCycle);
@@ -21,16 +35,33 @@ export default function patchAxios(axios, vue, options) {
    *  - cancelled if already sent
    *  - added to the `pendingRequests` hash if not, with a cancel function
    */
-  axios.interceptors.request.use((config) => {
+  axios.interceptors.request.use(config => {
     let { requestId, context } = config;
 
-    if (context && !requestId)
+    if (context && !requestId) {
       config.requestId = requestId = 'auto-generated-id-' + Math.floor(Math.random() * 100000000);
+    }
 
-    if (requestId) {
-      const source = CancelToken.source();
-      config.cancelToken = source.token;
-      requestManager.addRequest(requestId, context, source.cancel);
+    if (requestId && config.__cacheable === true) {
+      const cachedResponseData = cacheManager.getResponse(requestId);
+      if (cachedResponseData !== null) {
+        // Replace default request adapter so it returns the cached response immediately
+        config.adapter = () => {
+          return Promise.resolve({
+            data: cachedResponseData,
+            status: config.status,
+            statusText: config.statusText,
+            headers: config.headers,
+            config: Object.assign({ __cached: true }, config),
+            request: config
+          });
+        };
+      } else {
+        dispatchConfigEvent('onRequestStart', { config: config, payload: config });
+        const source = CancelToken.source();
+        config.cancelToken = source.token;
+        requestManager.addRequest(requestId, context, source.cancel);
+      }
     }
     return config;
   });
@@ -39,17 +70,24 @@ export default function patchAxios(axios, vue, options) {
    * Global response interceptor
    * Check for the `requestId` and remove it from the `pendingRequests` hash
    */
-  axios.interceptors.response.use((response) => {
-    const { requestId, context } = response.config;
-    if (requestId) {
+  axios.interceptors.response.use(response => {
+    const { requestId, context, __cached } = response.config;
+    if (requestId && !__cached) {
       requestManager.removeRequest(requestId, context);
+      if (response.config.__cacheable === true) {
+        cacheManager.addResponse(requestId, response);
+      }
+      dispatchConfigEvent('onRequestComplete', {
+        config: response.config,
+        payload: response
+      });
     }
     return response;
   });
 
   /**
    * Global axios method to cancel a single request by ID
-   * @param requestId: string
+   * @param requestIdOrContext: string
    * @param reason
    */
   axios.cancel = (requestIdOrContext, reason) => {
@@ -61,7 +99,21 @@ export default function patchAxios(axios, vue, options) {
   /**
    * Global axios method to cancel all requests
    */
-  axios.cancelAll = (reason) => {
-    requestManager.cancelAllRequests(reason)
+  axios.cancelAll = reason => {
+    requestManager.cancelAllRequests(reason);
+  };
+
+  /**
+   * Global axios method to clear all cached requests
+   */
+  axios.clearCache = requestId => {
+    cacheManager.removeResponse(requestId);
+  };
+
+  /**
+   * Global axios method to clear all cached requests
+   */
+  axios.clearAllCache = reason => {
+    requestManager.cancelAllRequests(reason);
   };
 }
